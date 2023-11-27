8f15ac5536b3c501f71746e8a79d32bb668f9950
Oleksandr Pupchenko <oleksandr.pupchenko@gmail.com>
2023-11-27 10:07:12 +0100

    Update requirements

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -18,7 +18,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG", "") == "True"
 
